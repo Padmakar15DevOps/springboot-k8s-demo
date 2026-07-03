17c7557bc08e1456aa981c8c6597708be8759e08
Padmakar Joshi <padmakarjoshi2718@gmail.com>
2026-07-03 11:35:39 +0530

    Initial Spring Boot Project

---
+++ src/main/java/com/padmakar/springboot_k8s_demo/SpringbootK8sDemoApplication.java
@@ -0,0 +1,12 @@
+package com.padmakar.springboot_k8s_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8sDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootK8sDemoApplication.class, args);
+    }
+}
